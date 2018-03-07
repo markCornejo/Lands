d3172e2ded5a5f58ccd091153d40cee13ee31b6f
mark cornejo <mark.cornejo.b@gmail.com>
2018-03-07 04:06:35 -0500

    LoginViewModel

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+
+    }
+}
--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -3,5 +3,15 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
     }
 }
