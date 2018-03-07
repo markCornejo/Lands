59f07b42648ebf0d46e06976c2c74be4acfc17ee
mark cornejo <mark.cornejo.b@gmail.com>
2018-03-06 23:01:01 -0500

    Login page

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lands.Infrastructure
+{
+
+    using ViewModels;
+
+
+    public class InstanceLocator
+    {
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+    }
+}
